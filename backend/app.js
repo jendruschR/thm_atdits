87687ab8b1c75dfa96cf2c76c511da6ed6050169
Marvin Roll <Marvin.Roll@de.bosch.com>
2023-01-13 22:17:20 +0100

    feat: pushing backend

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.use(cors());
+
+app.get("/message", function (req, res) {
+  res.send("Hello " + eval(req.query.q));
+  console.log(req.query.q);
+});
+
+app.listen(8080, function () {
+  console.log("Example app listening on port 8080!");
+});
